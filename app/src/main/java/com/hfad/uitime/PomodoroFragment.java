5356ab32e4e1836a79ba445c6d8d04043412da84
huytt268 <tuanhuytruong35@gmail.com>
2024-05-24 08:51:50 +0700

    bring chart to fragment

--- app/src/main/java/com/hfad/uitime/PomodoroFragment.java
+++ app/src/main/java/com/hfad/uitime/PomodoroFragment.java
@@ -1,5 +1,6 @@
 package com.hfad.uitime;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,24 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.hfad.uitime.views.TodoListActivity;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +42,14 @@ public class PomodoroFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private LineChart lineChart;
+    private List<String> xValues;
+    private EditText moodentry;
+    private Button btn_donerecord;
+    private ImageView back_arrow;
+    private SQLiteDatabase db;
+
+    private int idUser;
 
     public PomodoroFragment() {
         // Required empty public constructor
@@ -59,6 +86,46 @@ public class PomodoroFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pomodoro, container, false);
+        View chartview  = inflater.inflate(R.layout.fragment_pomodoro, container, false);
+        lineChart = chartview.findViewById(R.id.chart);
+        Description description = new Description();
+        description.setText("Mood Flow");
+        description.setPosition(150f, 15f);
+        lineChart.setDescription(description);
+        moodentry = chartview.findViewById(R.id.moodentry);
+        //lấy database
+        db = TodoListActivity.getDatabase();
+        idUser = this.getArguments().getInt("idUser");
+
+
+        xValues = Arrays.asList("1/5","11/5","21/5","31/5");
+
+        XAxis xAxis = lineChart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setValueFormatter(new IndexAxisValueFormatter(xValues));
+        xAxis.setLabelCount(4);
+        xAxis.setGranularity(1f);
+
+        YAxis yAxis = lineChart.getAxisLeft();
+        yAxis.setAxisMinimum(0f);
+        yAxis.setAxisMaximum(180f);
+        yAxis.setAxisLineWidth(2f);
+        yAxis.setAxisLineColor(android.R.color.holo_green_light);
+        yAxis.setLabelCount(5);
+
+        List<Entry> entries1 = new ArrayList<>();
+        entries1.add(new Entry(0,10f));
+        entries1.add(new Entry(1,10f));
+        entries1.add(new Entry(2,15f));
+        entries1.add(new Entry(3,45f));
+
+        LineDataSet dataSet1 = new LineDataSet(entries1, "Good");
+        dataSet1.setColor(android.R.color.holo_green_light);
+
+        LineData lineData = new LineData(dataSet1);
+        lineChart.setData(lineData);
+        lineChart.invalidate();
+
+        return chartview;
     }
 }
